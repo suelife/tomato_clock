2513ee13c600dd40d81d2a7fec9ceebc847aa716
windkuo <suelife@gmail.com>
2020-07-30 10:12:16 +0800

    [Add] is_change_work_and_break_time function

--- app_test.py
+++ app_test.py
@@ -1,9 +1,30 @@
 from tomato_test import TomatoClock
 
 
+def is_change_work_and_break_time():
+    contrast_talbe = {
+        "Y": True,
+        "N": False,
+    }
+
+    while True:
+        print("是否修改工作及休息時間?")
+        ans = input("(Y/N): ").upper()
+        if ans in ["Y", "N"]:
+            ans = contrast_talbe[ans]
+            break
+        
+    return ans
+
+
 def main():
     tomato = TomatoClock()
     tomato.summary()
+
+    if is_change_work_and_break_time():
+        tomato.set_work_minutes()
+        tomato.set_break_minutes()
+        
 
 
 if __name__ == "__main__":
