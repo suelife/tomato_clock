3cfd706cb3552f5f2f4a2a49f3bfa361cb7c334d
windkuo <suelife@gmail.com>
2020-07-30 09:22:09 +0800

    [Add] two file, app_test and tomato_test

---
+++ app_test.py
@@ -0,0 +1,10 @@
+from tomato_test import TomatoClock
+
+
+def main():
+    tomato = TomatoClock()
+    tomato.summary()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tomato_test.py
@@ -0,0 +1,53 @@
+import time
+import datetime
+
+
+class TomatoClock:
+    def __init__(self) -> None:
+        self.__WORK_MINUTES = 25
+        self.__BREAK_MINUTES = 5
+        self.__mode = "work"
+
+    def summary(self):
+        print(f"WORK_MINUTES: {self.__WORK_MINUTES}")
+        print(f"BREAK_MINUTES: {self.__BREAK_MINUTES}")
+        print(f"mode: {self.__mode}")
+    
+    def set_work_minutes(self, minutes):
+        self.__WORK_MINUTES = minutes
+
+    def set_break_minutes(self, minutes):
+        self.__BREAK_MINUTES = minutes
+    
+    def set_mode(self, mode):
+        self.__mode = mode
+
+    def timer(self):
+        start_time = time.perf_counter()
+        while True:
+            deviation_second = round(time.perf_counter() - start_time)
+            remaining_second = self.__WORK_MINUTES * 60 - deviation_second
+            if remaining_second <= -1:
+                break
+            else:
+                self.__prograssbar(remaining_second)
+            time.sleep(1)
+    
+    def __prograssbar(self, total_second):
+        minute, second = int(total_second/60), int(total_second%60)
+        remaining_time = datetime.time(minute=minute, second=second)
+        remaining_text = remaining_time.strftime("%M:%S")
+        print(f"剩餘時間: {remaining_text}", end="\r")
+
+    def notify_message(content):
+        print(f"{str(content)}")
+
+    
+if __name__ == "__main__":
+    tomato = TomatoClock()
+    print(help(tomato))
+    # tomato.summary()
+    # tomato.set_work_minutes(0.1)
+    # tomato.set_break_minutes(3)
+    # tomato.summary()
+    # tomato.timer()
