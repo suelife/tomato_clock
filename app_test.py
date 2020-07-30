1ccbf2ec6158baae25b309e8654d5cdde8430781
windkuo <suelife@gmail.com>
2020-07-30 10:26:44 +0800

    [Add] set_minutes function

--- app_test.py
+++ app_test.py
@@ -17,15 +17,30 @@ def is_change_work_and_break_time():
     return ans
 
 
+def set_minutes(text):
+    while True:
+        try:
+            minutes = float(input(f"輸入{text}時間(單位分鐘): "))
+            break
+        except ValueError:
+            print("請輸入數字")
+        except Exception as e:
+            print(e)
+
+    return minutes
+
+
 def main():
     tomato = TomatoClock()
     tomato.summary()
 
     if is_change_work_and_break_time():
-        tomato.set_work_minutes()
-        tomato.set_break_minutes()
+        tomato.set_work_minutes(set_minutes("工作"))
+        tomato.set_break_minutes(set_minutes("休息"))
+        tomato.summary()
         
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    set_minutes()
